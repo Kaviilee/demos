4d39b2d44167860d88c96b0342a0b189c3d8ff0f
Kaviilee <jiaweilee@126.com>
2020-11-25 23:45:04 +0800

    feat: add tools

--- pages/flexbox.vue
+++ pages/flexbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-box">
+  <div class="flex-box" :style="[{ flexDirection: activeDir }]">
     <div
       v-for="(img, index) in images"
       :key="img.title"
@@ -7,7 +7,23 @@
       class="item"
       :class="[{ 'active-item': activeKey === img.title }, `item${index + 1}`]"
       @click="handleClick(img.title)"
-    ></div>
+    >
+      <p class="index">{{ img.key }}</p>
+    </div>
+    <div class="tools">
+      <p>
+        <span
+          >direction:
+          <a
+            v-for="dir in directions"
+            :key="dir"
+            class="action"
+            @click="handleDirClick(dir)"
+            >{{ dir }}</a
+          >
+        </span>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -17,24 +33,38 @@ import { defineComponent, ref } from '@vue/composition-api'
 export default defineComponent({
   setup() {
     const activeKey = ref('item1')
+    const activeDir = ref('row')
     const images = [
       {
         title: 'Akali',
-        key: 'akali',
+        key: 'I',
       },
-      { title: 'Ahri', key: 'ahri' },
-      { title: 'all', key: 'all' },
-      { title: 'Evelynn' },
-      { title: 'KaiSa' },
+      { title: 'Ahri', key: 'II' },
+      { title: 'all', key: 'III' },
+      { title: 'Evelynn', key: 'IV' },
+      { title: 'KaiSa', key: 'V' },
     ]
+    const directions = ['row', 'row-reverse', 'column', 'column-reverse']
+
     const handleClick = (key: string) => {
-      activeKey.value = key
+      if (activeKey.value === key) {
+        activeKey.value = ''
+      } else {
+        activeKey.value = key
+      }
+    }
+
+    const handleDirClick = (dir: string) => {
+      activeDir.value = dir
     }
 
     return {
       handleClick,
       activeKey,
       images,
+      directions,
+      activeDir,
+      handleDirClick,
     }
   },
 })
@@ -42,11 +72,30 @@ export default defineComponent({
 
 <style lang="less" scoped>
 @images: '~assets/images/ahri.jpg', '~assets/images/akali.jpg',
-  '~assets/images/evelynn.jpg', '~assets/images/wallhaven-kwmood.jpg',
+  '~assets/images/wallhaven-kwmood.jpg', '~assets/images/evelynn.jpg',
   '~assets/images/kaisa.jpg';
 @colors: #cc9999, #ff9966, #cc3333, #cc9966, #cccc00;
 
 .flex-box {
+  .tools {
+    position: fixed;
+    width: 100%;
+    height: 3em;
+    text-align: center;
+    color: #fff;
+    margin-top: 4px;
+    top: 0;
+
+    .action {
+      cursor: pointer;
+      margin-right: 4px;
+
+      &:hover {
+        color: #cc0;
+      }
+    }
+  }
+
   display: flex;
   height: 100vh;
   width: 100%;
@@ -59,6 +108,8 @@ export default defineComponent({
     flex-direction: column;
     background: #ececec;
     text-align: center;
+    cursor: pointer;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     transition: font-size 0.2s cubic-bezier(0.61, -0.19, 0.7, -0.11),
       flex 0.2s cubic-bezier(0.61, -0.19, 0.7, -0.11), background 0.2s;
   }
@@ -66,6 +117,11 @@ export default defineComponent({
     flex: 3;
     font-size: 40px;
   }
+  .index {
+    font-size: 4em;
+    color: #fff;
+  }
+
   .loop(@data, @i: 1) when (@i =< length(@data)) {
     @item: extract(@data, @i);
 
